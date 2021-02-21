f6b6fa67aa8d7ed0c6b8e703b46c8a8ee29581be
jordins <jordi.nistal@gmail.com>
2021-02-21 19:02:26 +0100

    feat(beat): play sine wave within thread

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
-use tokio::time;
+use std::thread::{self, sleep};
+use std::time;
 
-
-
-#[tokio::main]
-async fn main() {
+fn main() {
     let bpm = 120;
     let time_signature = 4;
     let beat_player = get_beat_player(BeatPlayerSelection::Audio);
-    let interval = time::interval(time::Duration::from_millis(bpm_to_ms(bpm)));
-    tokio::pin!(interval);
 
     println!(
         "Starting metronome at {} bpm and {} beats per bar",
         bpm, time_signature
     );
-    
-    loop {
+
+    thread::spawn(move || loop {
         let mut beat_counter = 0;
         for _i in 0..time_signature {
-            interval.as_mut().tick().await;
+            sleep(time::Duration::from_millis(bpm_to_ms(bpm)));
             beat_counter += 1;
             beat_player.play(beat_counter);
         }
-    }
+    });
+
+    loop {}
 }
 
 fn bpm_to_ms(bpm: u64) -> u64 {
@@ -51,26 +49,17 @@ pub struct AudioBeatPlayer;
 impl BeatPlayer for AudioBeatPlayer {
     fn play(&self, beat_counter: u64) {
         println!("->{}", beat_counter);
-        
-        use rodio::{OutputStreamHandle, Source};
-        use std::fs::File;
-        use std::io::BufReader;
+        let beat_freq = if beat_counter == 1 { 640 } else { 440 };
 
-        let (stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+        let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
         let sink = rodio::Sink::try_new(&stream_handle).unwrap();
-
-        // Add a dummy source of the sake of the example.
-        let source = rodio::source::SineWave::new(440);
+        let source = rodio::source::SineWave::new(beat_freq);
         sink.append(source);
-        
 
-        let (stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+        let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
         let sink = rodio::Sink::try_new(&stream_handle).unwrap();
-
-        // Add a dummy source of the sake of the example.
-        let source = rodio::source::SineWave::new(470);
+        let source = rodio::source::SineWave::new(beat_freq);
         sink.append(source);
-        
 
         // use rodio::Source;
         // use std::fs::File;
@@ -81,17 +70,16 @@ impl BeatPlayer for AudioBeatPlayer {
         // let file = File::open("src/assets/sounds/mixkit-typewriter-hit-1362.wav").unwrap();
         // let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
         // stream_handle.play_raw(source.convert_samples()).unwrap();
-        
+
         // let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
         // // Load a sound from a file, using a path relative to Cargo.toml
         // let file = File::open("src/assets/sounds/mixkit-typewriter-hit-1362.wav").unwrap();
         // let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
         // stream_handle.play_raw(source.convert_samples()).unwrap();
-
     }
 }
 
-fn get_beat_player(audio: BeatPlayerSelection) -> Box<dyn BeatPlayer> {
+fn get_beat_player(audio: BeatPlayerSelection) -> Box<dyn BeatPlayer + Send + Sync> {
     match audio {
         BeatPlayerSelection::Audio => Box::new(AudioBeatPlayer {}),
         BeatPlayerSelection::Text => Box::new(TextBeatPlayer {}),
